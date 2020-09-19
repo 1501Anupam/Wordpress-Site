c6b18114de3ac7068c7f05df5d0862fec865c292
Anupam <43044405+1501Anupam@users.noreply.github.com>
2020-09-20 04:27:37 +0530

    Update header.php

--- Hogwarts/header.php
+++ Hogwarts/header.php
@@ -2,20 +2,21 @@
 <html lang="en">
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<!-- <title>Fictional Site</title> -->
 <?php wp_head(); ?>
 </head>
 <body>
 <header class="site-header" style="background: #000;">
       <div class="container">
         <h1 class="school-logo-text float-left">
-          <a href="#"><strong>Fictional</strong> University</a>
+          <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
         </h1>
         <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
             <ul>
-              <li><a href="#">About Us</a></li>
+              <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
