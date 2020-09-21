148f63812c87e804b81c7d0d97d0fd3a62404c15
Anupam <43044405+1501Anupam@users.noreply.github.com>
2020-09-21 14:28:35 +0530

    Update header.php

--- Hogwarts/header.php
+++ Hogwarts/header.php
@@ -3,9 +3,11 @@
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <!-- <title>Fictional Site</title> -->
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta name="viewport" content="width= device-width, initial-scale=1">
 <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <header class="site-header" style="background: #000;">
       <div class="container">
         <h1 class="school-logo-text float-left">
@@ -15,6 +17,11 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
+          <?php 
+          // wp_nav_menu(array(
+          //     'theme_location' => 'headerMenuLocation'
+          // ));
+          ?>
             <ul>
               <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
